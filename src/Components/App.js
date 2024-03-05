f8485518c1152d1944b2ef4cddea37d8dfbb8c3e
Kim Drezdon <kim.drezdon@gmail.com>
2024-03-05 16:58:52 -0600

    Connect app to Spotify API

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,26 +1,46 @@
 import styles from '../styles/App.module.css';
-import { useState } from 'react';
-
-const searchResultsArray = [
-	{ song: 'Lateralus', artist: 'Tool', album: 'Lateralus', id: '0943' },
-	{
-		song: 'Rx Queen',
-		artist: 'Deftones',
-		album: 'White Pony',
-		id: '2309',
-	},
-	{
-		song: 'A History of Bad Men',
-		artist: 'Melvins',
-		album: 'A Senile Animal',
-		id: '1230',
-	},
-];
+import { useEffect, useState } from 'react';
+import config from '../config';
 
 export default function App() {
-	const [searchInput, setSearchInput] = useState('');
-	const [searchResults, setSearchResults] = useState(searchResultsArray);
+	const [searchInput, setSearchInput] = useState('tool');
+	const [searchResults, setSearchResults] = useState([]);
 	const [playlist, setPlaylist] = useState([]);
+
+	useEffect(
+		function () {
+			async function fetchSongs() {
+				// const res1 = await fetch(
+				// 	'https://accounts.spotify.com/api/token',
+				// 	{
+				// 		body: `grant_type=client_credentials&client_id=${config.MY_CLIENT_ID}&client_secret=${config.MY_SECRET}`,
+				// 		headers: {
+				// 			'Content-Type': 'application/x-www-form-urlencoded',
+				// 		},
+				// 		method: 'POST',
+				// 	}
+				// );
+				// const auth = await res1.json();
+				// config.MY_TOKEN = auth.access_token
+
+				const res2 = await fetch(
+					`https://api.spotify.com/v1/search?q=${searchInput}&type=track&limit=10`,
+					{
+						headers: {
+							Authorization: `Bearer  ${config.MY_TOKEN}`,
+						},
+						method: 'GET',
+					}
+				);
+				const data = await res2.json();
+				console.log(data.tracks.items);
+				setSearchResults(data.tracks.items);
+			}
+
+			fetchSongs();
+		},
+		[searchInput]
+	);
 
 	const handleAddTrack = trackObject => {
 		setPlaylist(playlist => [...playlist, trackObject]);
@@ -113,10 +133,10 @@ function SearchResults({ searchResults, onAddTrack }) {
 					key={track.id}
 				>
 					<div>
-						<h3>{track.song}</h3>
+						<h3>{track.name}</h3>
 						<h4>
-							<span>{track.artist}</span>-
-							<span>{track.album}</span>
+							<span>{track.artists[0].name}</span> -
+							<span>{track.album.name}</span>
 						</h4>
 					</div>
 					<button
@@ -140,10 +160,10 @@ function Playlist({ playlist, onRemoveTrack }) {
 					key={track.id}
 				>
 					<div>
-						<h3>{track.song}</h3>
+						<h3>{track.name}</h3>
 						<h4>
-							<span>{track.artist}</span>-
-							<span>{track.album}</span>
+							<span>{track.artists[0].name}</span> -
+							<span>{track.album.name}</span>
 						</h4>
 					</div>
 					<button
